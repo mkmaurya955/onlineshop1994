1a5af8d33e176a743b43eedf67899589e13a3b74
manish <mkmaurya955@gmail.com>
2020-03-17 15:51:02 +0530

    make it better

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -161,10 +161,10 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {    
      'CacheControl': 'max-age=86400',
      }
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-STATICFILES_FINDERS = (           
-    'django.contrib.staticfiles.finders.FileSystemFinder',    
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+#STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+#STATICFILES_FINDERS = (           
+#    'django.contrib.staticfiles.finders.FileSystemFinder',    
+#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#)
 AWS_DEFAULT_ACL = None
